0b02f9c78577336cb7f54fb9ac6fb13c07a109d8
gabri3l0 <42877617+gabri3l0@users.noreply.github.com>
2020-04-12 18:49:21 -0500

    Create, modify algorithm

--- P6/Calidad6.py
+++ P6/Calidad6.py
@@ -8,7 +8,8 @@ Codigo de Honor:    Doy mi palabra que he realizado esta actividad con integrida
 
 import math #Base
 
-E = 0.00001 #Base
+E = 0.0000001 #Base
+# E = 0.00001 #Base
 n = 0 #Base
 lista = [] #Base
 
@@ -20,21 +21,24 @@ class ListaC: #Base #Class
     def calcularP(self,num_seg): #Modified #Class #Deleted
         p = 0 #Modified #Class #Deleted
         for i in range(0,num_seg+1): #Modified #Class #Deleted
-        	xi = (x/num_seg)*i #Modified #Class #Deleted
-        	intregral = (1 + (xi**2/dof)) #Modified #Class #Deleted
-        	integral2 = (intregral**-((dof+1)/2)) #Added #Class #Deleted
-        	fx = (integral2*gamma) #Modified #Class #Deleted
-        	
-        	if(i==0 or i==num_seg): #Modified #Class #Deleted
-        		termsData = ((w*1*fx)/3) #Modified #Class #Deleted
-        		p += termsData #Modified #Class #Deleted
-        	elif (i%2==0): #Added #Class #Deleted
-        		termsData = ((w*2*fx)/3) #Added #Class #Deleted
-        		p += termsData #Added #Class #Deleted
-        	elif (i%2==1): #Added #Class #Deleted
-        		termsData = ((w*4*fx)/3) #Added #Class #Deleted
-        		p += termsData #Added #Class 
-        return p #Added #Class 
+            xi = (x/num_seg)*i #Modified #Class #Deleted
+            intregral = (1 + (xi**2/dof)) #Modified #Class #Deleted
+            integral2 = (intregral**-((dof+1)/2)) #Added #Class #Deleted
+            fx = (integral2*gamma) #Modified #Class #Deleted
+            # print(fx)
+            if(i==0 or i==num_seg): #Modified #Class #Deleted
+               termsData = ((w*1*fx)/3) #Modified #Class #Deleted
+               p += termsData #Modified #Class #Deleted
+            elif (i%2==0): #Added #Class #Deleted
+               termsData = ((w*2*fx)/3) #Added #Class #Deleted
+               p += termsData #Added #Class #Deleted
+            elif (i%2==1): #Added #Class #Deleted
+               termsData = ((w*4*fx)/3) #Added #Class #Deleted
+               p += termsData #Added #Class 
+        return p #Added #Class
+    #Funcion de agregar valor de la lista
+    def agregarError(self,data): #Base #Class #Deleted
+        return error.append(data) #Base #Class #Deleted 
 
 l1 = ListaC() #Base
 
@@ -48,23 +52,69 @@ while True: #Base
     except EOFError: #Added
         break #Added
 
-for i in range(n): #Added
-	num_seg = 10 #Base
-	x = float(lista[i][0]) #Added
-	dof = float(lista[i][1]) #Added
-	w = x / num_seg #Added
+for i in range(1): #Added
+# for i in range(n): #Added
+    signo = False
+    num_seg = 10 #Base
+    p = float(lista[i][0]) #Added
+    dof = float(lista[i][1]) #Added
+    x = 1
+    d = 0.5
+    w = x / num_seg #Added
+    error = [] #Base
 
-	gamma = math.gamma((dof+1)/2)/(((dof*math.pi)**0.5)*math.gamma(dof/2)) #Added
+    gamma = math.gamma((dof+1)/2)/(((dof*math.pi)**0.5)*math.gamma(dof/2)) #Added
 
-	pE = l1.calcularP(num_seg) #Added
-	pA = 0 #Added
+    pE = l1.calcularP(num_seg) #Added
+    # pE = 0
 
-	while (abs(pE-pA)>=E): #Added
-		num_seg *= 2 #Added
-		w = x / num_seg #Added
-		pA = l1.calcularP(num_seg) #Added
+    print(pE,x)
+    if (p-pE)<0:
+        x -= d
+        signo = False
+    if (p-pE)>0:
+        x += d
+        signo = True
+    l1.agregarError(p-pE) #Added
+    print(pE,x)
 
 
-	print("0 to x\t\tdof\t\tActual Value") #Added
-	print(x,"\t\t",int(dof),"\t\t",round((pA),5)) #Added
+    cont = 1
+    while (abs(p-pE)>=E): #Added
+        w = x / num_seg #Added
+        pE = l1.calcularP(num_seg) #Added
+
+        signoTemp = signo
+
+        if ((p-pE) not in error):
+            if (p-pE)<0:
+                signo = False
+                if (signo != signoTemp):
+                    d /= 2
+                x -= d
+            if (p-pE)>0:
+                signo = True
+                if (signo != signoTemp):
+                    d /= 2
+                x += d
+            l1.agregarError(p-pE) #Added
+        else: 
+            if (p-pE)<0:
+                signo = False
+                d /= 2
+                x -= d
+            if (p-pE)>0:
+                signo = True
+                d /= 2
+                x += d
+        cont += 1
+        # print(error)
+        # if(cont==15):
+            # break
+        print(pE,x)
+        # print(round(x,6))
+    print('----')
+
+	# print("0 to x\t\tdof\t\tActual Value") #Added
+	# print(x,"\t\t",int(dof),"\t\t",round((pA),5)) #Added
 
