7b3f09483fd76e270b6dc285ed15343208c0879d
gabri3l0 <42877617+gabri3l0@users.noreply.github.com>
2020-04-17 12:59:38 -0500

    Fix all bugs

--- P6/Calidad6.py
+++ P6/Calidad6.py
@@ -8,7 +8,7 @@ Codigo de Honor:    Doy mi palabra que he realizado esta actividad con integrida
 
 import math #Base
 
-E = 0.000000000000001 #Base
+E = 0.000000001 #Base
 n = 0 #Base
 lista = [] #Base
 
@@ -17,22 +17,21 @@ class ListaC: #Base #Class
     def agregar(self,data): #Base #Class #Deleted
         return lista.append(data) #Base #Class #Deleted
     #Funcion para calcular la P
-    def calcularP(self,num_seg): #Modified #Class #Deleted
-        p = 0 #Modified #Class #Deleted
-        for i in range(0,num_seg+1): #Modified #Class #Deleted
-            xi = (x/num_seg)*i #Modified #Class #Deleted
-            intregral = (1 + (xi**2/dof)) #Modified #Class #Deleted
-            integral2 = (intregral**-((dof+1)/2)) #Base #Class #Deleted
-            fx = (integral2*gamma) #Modified #Class #Deleted
-            # print(fx)
-            if(i==0 or i==num_seg): #Modified #Class #Deleted
-               termsData = ((w*1*fx)/3) #Modified #Class #Deleted
-               p += termsData #Modified #Class #Deleted
-            elif (i%2==0): #Base #Class #Deleted
-               termsData = ((w*2*fx)/3) #Base #Class #Deleted
-               p += termsData #Base #Class #Deleted
-            elif (i%2==1): #Base #Class #Deleted
-               termsData = ((w*4*fx)/3) #Base #Class #Deleted
+    def calcularP(self,num_seg): #Base #Class #Deleted
+        p = 0 #Base #Class #Deleted
+        for i in range(0,num_seg+1): #Base #Class
+            xi = (x/num_seg)*i #Base #Class
+            intregral = (1 + (xi**2/dof)) #Base #Class 
+            integral2 = (intregral**-((dof+1)/2)) #Base #Class 
+            fx = (integral2*gamma) #Base #Class 
+            if(i==0 or i==num_seg): #Base #Class 
+               termsData = (((w/3)*1*fx)) #Base #Class 
+               p += termsData #Base #Class 
+            elif (i%2==0): #Base #Class 
+               termsData = (((w/3)*2*fx)) #Base #Class 
+               p += termsData #Base #Class 
+            elif (i%2==1): #Base #Class 
+               termsData = (((w/3)*4*fx)) #Base #Class 
                p += termsData #Base #Class 
         return p #Base #Class
     #Funcion de agregar valor de la lista
@@ -44,20 +43,23 @@ l1 = ListaC() #Base
 while True: #Base
     try: #Base
         line = input() #Base
+        if (line.isupper() or line.islower()): #Added
+            print('El dataset no esta limpio contiene letras') #Added
+            exit(1) #Added
         line = line.split() #Base
         l1.agregar(line) #Base
         n += 1 #Base
     except EOFError: #Base
         break #Base
 
-print("p\t\tdof\t\tActual Value x") #Base
+print("p\t\tdof\t\tActual Value x") #Modified
 
 for i in range(n): #Base
 
     signo = None #Added
-    num_seg = 20 #Base
-    p = float(lista[i][0]) #Base
-    dof = float(lista[i][1]) #Base
+    num_seg = 10 #Base
+    p = float(lista[i][0]) #Modified
+    dof = float(lista[i][1]) #Modified
     x = 1 #Added
     d = 0.5 #Added
     error = [] #Added
@@ -104,4 +106,4 @@ for i in range(n): #Base
 
         pE = l1.calcularP(num_seg) #Added
 
-    print(p,"\t\t",int(dof),"\t\t",round(x,5)) #Base
+    print(p,"\t\t",int(dof),"\t\t",round(x,5)) #Modified
