6d5247d572c131cf45556391fd5c5930bd980056
gabri3l0 <42877617+gabri3l0@users.noreply.github.com>
2020-04-29 02:20:36 -0500

    Fix 6 and Fix 7, falta terminar de editar los comentarios

--- P6/Calidad6.py
+++ P6/Calidad6.py
@@ -8,7 +8,7 @@ Codigo de Honor:    Doy mi palabra que he realizado esta actividad con integrida
 
 import math #Base
 
-E = 0.000001 #Base
+E = 0.000000000001 #Base
 n = 0 #Base
 lista = [] #Base
 
@@ -16,27 +16,27 @@ class ListaC: #Base #Class
     #Funcion de agregar valor de la lista
     def agregar(self,data): #Base #Class #Deleted
         return lista.append(data) #Base #Class #Deleted
-    #Funcion para calcular la P
-    def calcularP(self,num_seg): #Base #Class #Deleted
-        p = 0 #Base #Class #Deleted
-        for i in range(0,num_seg+1): #Base #Class
-            xi = (x/num_seg)*i #Base #Class
-            intregral = (1 + (xi**2/dof)) #Base #Class 
+    def calcularP(self,num_seg,w): #Base #Class #Deleted
+        newW = 0 #Added
+        p= 0 #Added
+        for i in range(num_seg + 1): #Base #Class
+
+            intregral = (1 + (newW**2/dof)) #Base #Class 
+
             integral2 = (intregral**-((dof+1)/2)) #Base #Class 
+
             fx = (integral2*gamma) #Base #Class 
+
             if(i==0 or i==num_seg): #Base #Class 
                termsData = (((w/3)*1*fx)) #Base #Class 
-               p += termsData #Base #Class 
             elif (i%2==0): #Base #Class 
                termsData = (((w/3)*2*fx)) #Base #Class 
-               p += termsData #Base #Class 
             elif (i%2==1): #Base #Class 
                termsData = (((w/3)*4*fx)) #Base #Class 
-               p += termsData #Base #Class 
-        return float(p) #Base #Class
-    #Funcion de agregar valor de la lista
-    def agregarError(self,data): #Added #Class 
-        return error.append(data) #Added #Class 
+
+            p += termsData #Added
+            newW += w #Added
+        return p #Base #Class
 
 l1 = ListaC() #Base
 
@@ -54,63 +54,41 @@ while True: #Base
 
 print("p\t\tdof\t\tActual Value x") #Modified
 
-for i in range(3): #Base
+for i in range(n): #Base
 
-    signo = None #Added
-    num_seg = 10 #Base
     p = float(lista[i][0]) #Modified
     dof = float(lista[i][1]) #Modified
     x = 1 #Added
     d = 0.5 #Added
-    error = [] #Added
+    pE = 0 #Added
+    flag = False #Added  
 
     gamma = math.gamma((dof+1)/2)/(((dof*math.pi)**0.5)*math.gamma(dof/2)) #Base
 
-    w = x / num_seg #Base
+    while (abs(pE - p)>E): #Base
+        pA = 1 #Added
+        num_seg = 5 #Added
 
-    pE = l1.calcularP(num_seg) #Base
+        while (abs(pA-pE)>=E): #Base
 
-    while (abs(p-pE)>=E): #Base
+            num_seg = num_seg * 2 #Added
 
+            w = x / num_seg #Base
 
-        if signo is None: #Added
-            if (p-pE)<0: #Added
-                signo = False #Added
-            else: #Added
-                signo = True #Added
+            pA = pE #Added
 
-        signoTemp = signo #Added
+            pE = l1.calcularP(num_seg,w) #Added
 
-        if ((p-pE) not in error): #Added
-            if (p-pE)<0: #Added
-                signo = False #Added
-                if (signo != signoTemp): #Added
-                    d /= 2 #Added
-                x -= d #Added
+        if flag: #Added
             if (p-pE)>0: #Added
-                signo = True #Added
-                if (signo != signoTemp): #Added
-                    d /= 2 #Added
-                x += d #Added
-            l1.agregarError(p-pE) #Added
+                d /= 2 #Added
+            if d == 0: #Added
+                d = 2 #Added
+
+        if pE > p: #Added
+            x -= d #Added
         else: #Added
-            if (p-pE)<0: #Added
-                signo = False #Added
-                d /= 2 #Added
-                x -= d #Added
-            if (p-pE)>0: #Added
-                signo = True #Added
-                d /= 2 #Added
-                x += d #Added
+            x += d #Added
+        flag = True #Added
 
-        num_seg = num_seg * 2;    
-        # print(num_seg)
-        # print(w)
-        # print(abs(p-pE))
-        w = x / num_seg #Added
-
-
-        pE = l1.calcularP(num_seg) #Added
-
-
-    print(p,"\t\t",int(dof),"\t\t",round(x,5)) #Modified
+    print(p,"\t\t",int(dof),"\t\t",x) #Modified
--- P7/Calidad7.py
+++ P7/Calidad7.py
@@ -8,7 +8,8 @@ Codigo de Honor:    Doy mi palabra que he realizado esta actividad con integrida
 
 import math #Base
 
-E = 0.0001 #Base
+E = 0.000001 #Base
+# E = 0.0000000000001 #Base
 n = 0 #Base
 lista = [] #Base
 
@@ -16,28 +17,91 @@ class ListaC: #Base #Class
     #Funcion de agregar valor de la lista
     def agregar(self,data): #Base #Class #Deleted
         return lista.append(data) #Base #Class #Deleted
-    #Funcion para calcular la P
-    def calcularP(self,num_seg): #Base #Class #Deleted
-        p = 0 #Base #Class #Deleted
-        for i in range(0,num_seg+1): #Base #Class
-            xi = (x/num_seg)*i #Base #Class
-            intregral = (1 + (xi**2/dof)) #Base #Class 
-            integral2 = (intregral**-((dof+1)/2)) #Base #Class 
-            fx = (integral2*gamma) #Base #Class 
-            if(i==0 or i==num_seg): #Base #Class 
-               termsData = (((w/3)*1*fx)) #Base #Class 
-               p += termsData #Base #Class 
-            elif (i%2==0): #Base #Class 
-               termsData = (((w/3)*2*fx)) #Base #Class 
-               p += termsData #Base #Class 
-            elif (i%2==1): #Base #Class 
-               termsData = (((w/3)*4*fx)) #Base #Class 
-               p += termsData #Base #Class 
-        return p #Base #Class
-    #Funcion de agregar valor de la lista
-    def agregarError(self,data): #Added #Class 
-        return error.append(data) #Added #Class 
+    def calcularP(self,dof,x):
+        gamma = math.gamma((dof+1)/2)/(((dof*math.pi)**0.5)*math.gamma(dof/2))
 
+        pA = 1
+        pE = 0
+        num_seg = 5
+
+        #Inicio del while del pg5 
+        while (abs(pA-pE)>=E): #Base
+
+            num_seg = num_seg * 2;
+
+            w = x / num_seg #Base
+
+            pA = pE #WTF
+
+            # pE = l1.calcularP(num_seg,w)
+            newW = 0 #WTF
+            p = 0
+            for i in range(num_seg + 1): #Base #Class
+
+                intregral = (1 + (newW**2/dof)) #Base #Class 
+
+                integral2 = (intregral**-((dof+1)/2)) #Base #Class 
+
+                fx = (integral2*gamma) #Base #Class 
+
+                if(i==0 or i==num_seg): #Base #Class 
+                   termsData = (((w/3)*1*fx)) #Base #Class 
+                elif (i%2==0): #Base #Class 
+                   termsData = (((w/3)*2*fx)) #Base #Class 
+                elif (i%2==1): #Base #Class 
+                   termsData = (((w/3)*4*fx)) #Base #Class 
+
+                p += termsData
+                newW += w   
+            # return p #Base #
+            pE = p
+        return pE
+    def calcularX(self,rXY,n,rCuadrada):
+        top = (abs(rXY)*math.sqrt(n-2))
+        bot = math.sqrt(1-rCuadrada)
+        x = top / bot
+        return x
+    def calcularRigth(self,xK,promX,n):
+        top = (xK-promX)**2
+        bot = 0
+        for data in range(n):
+            bot += (float(lista[data][testColumna[i][0]])-promX)**2
+        func = math.sqrt(1+(1/n)+(top**2/bot))
+        return func
+    def funcionT(sefl,dof,p):
+        x = 1 
+        d = 0.5
+        pE = 0
+        flag = False
+        pE = 0
+
+        #Inicio while pg 6
+        while (abs(pE - p)>E): #Base
+            pA = 1
+            num_seg = 5  
+
+        #Inicio del while del pg5 
+            pE = l1.calcularP(dof,x)
+        #Fin del while del pg5 
+
+            if flag:
+                if (p-pE)>0:
+                    d /= 2
+                if d == 0:
+                    d = 2
+
+            if pE > p:
+                x -= d
+            else:
+                x += d
+            flag = True
+        return x
+    def desvstd(self,n,y,x,b0,b1):
+        sumatoria = 0
+        for data in range(n):
+            sumatoria += (float(lista[data][y])-b0-(b1*float(lista[data][x])))**2
+        desvstd = math.sqrt((1/(n-2))*sumatoria)
+        return desvstd
 #---------------------------------------------------------
     #Funcion de calcular el promedio del test 
     def promedio(self,param1): #Modified #Class #Deleted
@@ -103,8 +167,6 @@ while True: #Base
     except EOFError: #Base
         break #Base
 
-# print("p\t\tdof\t\tActual Value x") #Modified
-
 testColumna = [[1,3],[1,4],[1,2],[1,2]]
 
 print("      r \t r^2 \t  B0 \t\t B1\t\tyk")
@@ -117,12 +179,6 @@ for i in range(2): #Base
     yK = 0
     xK = 386
     n = len(lista)
-
-    signo = None #Added
-    p = 0
-
-    # d = 0.5 #Added
-    # error = [] #Added
 
     l1 = ListaC()
 
@@ -143,85 +199,31 @@ for i in range(2): #Base
     rCuadrada = l1.rCuadrada(rXY) 
     yK = l1.yK(bCero,bUno,386)
 
-    data1 = (abs(rXY)*math.sqrt(n-2))
-    data2 = math.sqrt(1-rCuadrada)
-    test = data1 / data2
+    x = l1.calcularX(rXY,n,rCuadrada)
 
-    num_seg = 10 
     dof = n-2
-    x = test
-    w = x / num_seg
 
-    gamma = math.gamma((dof+1)/2)/(((dof*math.pi)**0.5)*math.gamma(dof/2))
+    pE = l1.calcularP(dof,x)
 
-    pA = 0 
-    # pE = l1.calcularP(num_seg) 
-    pE = 1
+    tail = 1-2*pE
 
+    rigth = l1.calcularRigth(xK,promX,n)
 
-
-    t=0
-    while (abs(pE-pA)>E): #Added
-        pE = l1.calcularP(num_seg) #Added
-
-        num_seg *= 2 #Added
-
-        w = x / num_seg #Added
-
-        pA = l1.calcularP(num_seg) #Added
+    desvstd = l1.desvstd(n,testColumna[i][1],testColumna[i][0],bCero,bUno)
 
 
-    tail = 1-2*pA
+    # p = 0.349991
+    p = 0.35
+    t = l1.funcionT(dof,p)
 
-    # print(test)
-    
-
-    print("{:.9f}".format(rXY),"{:.9f}".format(rCuadrada),"{:.9f}".format(bCero),"{:.9f}".format(bUno),"{:.9f}".format(yK))
-    print('Tails',tail)
-    print('')
-    # break
-
-
-
-    # num_seg = num_seg * 2;
-    # w = x / num_seg #Base
-
-    # pE = l1.calcularP(num_seg) #Base
-
-    # while (abs(p-pE)>=E): #Base
-
-    #     if signo is None: #Added
-    #         if (p-pE)<0: #Added
-    #             signo = False #Added
-    #         else: #Added
-    #             signo = True #Added
-
-    #     signoTemp = signo #Added
-
-    #     if ((p-pE) not in error): #Added
-    #         if (p-pE)<0: #Added
-    #             signo = False #Added
-    #             if (signo != signoTemp): #Added
-    #                 d /= 2 #Added
-    #             x -= d #Added
-    #         if (p-pE)>0: #Added
-    #             signo = True #Added
-    #             if (signo != signoTemp): #Added
-    #                 d /= 2 #Added
-    #             x += d #Added
-    #         l1.agregarError(p-pE) #Added
-    #     else: #Added
-    #         if (p-pE)<0: #Added
-    #             signo = False #Added
-    #             d /= 2 #Added
-    #             x -= d #Added
-    #         if (p-pE)>0: #Added
-    #             signo = True #Added
-    #             d /= 2 #Added
-    #             x += d #Added
-
-    #     w = x / num_seg #Added
-
-    #     pE = l1.calcularP(num_seg) #Added
-
-    # print(p,"\t\t",int(dof),"\t\t",round(x,5)) #Modified
+    print('Test ',(i+1))
+    print('rxy',"{:.9f}".format(rXY))
+    print('r^2',"{:.9f}".format(rCuadrada))
+    print('tail area',round(tail,10))
+    print('B0',"{:.9f}".format(bCero))
+    print('B1',"{:.9f}".format(bUno))
+    print('yk',"{:.9f}".format(yK))
+    print('Range',(t*desvstd*rigth))
+    print('UPI',(yK+(t*desvstd*rigth)))
+    print('LPI',(yK-(t*desvstd*rigth)))
+    print('----')
